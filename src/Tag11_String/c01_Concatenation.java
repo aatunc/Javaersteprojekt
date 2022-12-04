33150b2da1a3157cf583853111a75d07ba86daa7
A Tunc <106016458+aatunc@users.noreply.github.com>
2022-12-04 18:43:49 +0100

    Java

---
+++ src/Tag11_String/c01_Concatenation.java
@@ -0,0 +1,14 @@
+package Tag11_String;
+
+public class c01_Concatenation {
+    public static void main(String[] args) {
+
+        String str1="Java";
+        String str2="Schön";
+        boolean bl= true;
+        int zahl= 20;
+        double dbl= 3.54;
+
+        System.out.println(str1+bl+zahl);
+    }
+}
